a06c4c3d266df874e3229b3bf17010e856e6eaf7
Farreset <ericfarrejove@gmail.com>
2023-11-24 15:49:36 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'TAG',
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blue,
+              title: const Text('TAG'),
+            ),
+            body: Center(
+              child: Image(image: AssetImage('image/background.png')),
+            )
+
+        )
+    );
+  }
+}
+
