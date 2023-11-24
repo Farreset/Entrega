cb7f0064ef95fb1afbf49ba214c0b950567406d7
Farreset <ericfarrejove@gmail.com>
2023-11-24 17:09:53 +0100

    bola
    bola

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -10,18 +11,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TAG',
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.blue,
-              title: const Text('TAG'),
-            ),
-            body: const Center(
-              child: Image(image: AssetImage('../image/background.png')),
-            )
-
-        )
+      title: 'BOLA MAGICA',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('BOLA MAGICA'),
+        ),
+        body: Center(
+          child: MagicBall(),
+        ),
+      ),
     );
   }
 }
 
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int random = 1;
+
+  void generateRandomNumber() {
+    final newRandom = Random().nextInt(5) + 1; // que los numeros sean random del 1 al 5
+    setState(() {
+      random = newRandom;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell( // Area rectagular que responde a los toques
+      onTap: () {
+        generateRandomNumber(); // funcion que randomiza el numero
+        print('Numero de foto es $random');
+      },
+      child: Image.asset(
+        '../image/ball$random.png', // Ajusta la ruta según tu estructura de carpetas
+        width: 1200,
+        height: 1200,
+      ),
+    );
+  }
+}
